fb032e1eaf154c79fb8e0d50dfe3b686ac8ddfcd
AKASH YADAV <yaak73749@gmail.com>
2026-08-07 00:02:27 +0530

    added automatic refresh option in APP

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'
 
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-})
+// Automatically registers and activates new updates
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
